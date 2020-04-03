b662efdb160725337353dc0aee103a79ce970858
ViktorKup <argon11@email.cz>
2020-04-03 22:48:46 +0200

    .

--- SpawnEnemyWaves/SpawnEnemyWaves.cs
+++ SpawnEnemyWaves/SpawnEnemyWaves.cs
@@ -1,10 +1,52 @@
 ﻿// Script si delam pouze pro sebe, nedelam ho do zadneho skolniho projektu
+using UnityEngine.AI
 
-
-namespace DefaultNamespace
+public class SpawnEnemyWaves : MonoBehaviour
 {
-    public class SpawnEnemyWaves
+    
+    public enum SpawnState{Spawning, Waiting, Counting}
+    
+    [System.Serializable]
+    public class Wave
     {
-        
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+    
+    public Transform[] spawnpoints;
+    
+    public float timeBetweenWaves = 5f;
+    private float waveCountdown;
+    
+    private SpawnState state = SpawnState.Counting;
+
+    void Update()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+    
+    void SpawnEnemy(Transform enemy)
+    {
+        Transform spawnpoint = spawnpoints[Random.Range(0, spawnpoints.Length)];
+        Instantiate(enemy, spawnpoint.position, spawnpoint.rotation);
+    }
+    
+    bool IsEnemyAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectsWithTag("enemy").Length == 0f)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
